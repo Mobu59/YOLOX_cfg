dc7a27b2f64f4b019fe326b8d561279c98339e86
Your Name <you@example.com>
2022-09-13 16:16:43 +0800

    first commit

---
+++ yolox/data/datasets/goods_classes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+GOODS_CLASSES = (
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5", 
+    "6",
+    "7",
+    "8",
+)
